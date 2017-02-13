c8bf9d5d12fed1cb2d7cd990edf0cf8e6aa09d49
Carlos Cupeiro <carlos.cupeiro@grupocmc.es>
2017-02-13 09:23:46 +0100

    Pagina 24 suscripción a temas (5.1.3.3)

---
+++ app/src/main/java/org/masterupv/carloscupeiro/eventos/main/domain/model/EventosFCMService.java
@@ -0,0 +1,31 @@
+package org.masterupv.carloscupeiro.eventos.main.domain.model;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static org.masterupv.carloscupeiro.eventos.main.domain.model.EventosAplicacion.mostrarDialogo;
+
+/**
+ * Created by carlos.cupeiro on 13/02/2017.
+ */
+
+public class EventosFCMService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        if (remoteMessage.getData().size() > 0) {
+            String evento="";
+            evento ="Evento: "+remoteMessage.getData().get("evento")+ "\n";
+            evento = evento + "Día: "+ remoteMessage.getData().get("dia")+ "\n";
+            evento = evento +"Ciudad: "+
+                    remoteMessage.getData().get("ciudad")+"\n";
+            evento = evento +"Comentario: "
+                    +remoteMessage.getData().get("comentario");
+            mostrarDialogo(getApplicationContext(), evento);
+        } else {
+            if (remoteMessage.getNotification() != null) {
+                mostrarDialogo(getApplicationContext(),
+                        remoteMessage.getNotification().getBody());
+            }
+        }
+    }
+}
